da5e79a028695ac380f026bacc27947970efa54c
Anthony Peng <anthony_peng@brown.edu>
2020-07-09 20:42:42 -0400

    Updates

---
+++ precache-manifest.f0e085a2e139da2145d16f5b16f53b99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb59a3e00c0a1326000850ec5fa2e348",
+    "url": "/react-github-portfolio/index.html"
+  },
+  {
+    "revision": "617a7898e12c722ef534",
+    "url": "/react-github-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b513f70bf8f9288b14de",
+    "url": "/react-github-portfolio/static/js/2.60151a52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-portfolio/static/js/2.60151a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617a7898e12c722ef534",
+    "url": "/react-github-portfolio/static/js/main.4aabe6ca.chunk.js"
+  },
+  {
+    "revision": "f9810a347c772ce5fc06",
+    "url": "/react-github-portfolio/static/js/runtime-main.57f926e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
